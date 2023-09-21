99f02d8960e7cf403f2fbce2f4b793f97dacc109
imamprasetyo28 <143422371+imamprasetyo28@users.noreply.github.com>
2023-09-21 10:57:14 +0700

    update new file

---
+++ 3.Tipe-data.js
@@ -0,0 +1,32 @@
+// syntax variable assign value
+let nama = "Rijal"          // string
+var menikah = true          // boolean
+const umur = 17             // number
+
+// array
+let murid = [ ["Imam", true, 19], ["Faizal", false, 19], "Bintang", "Faisal" ] 
+    // square bracket = kurung kotak
+
+// const tidak dapat di assign ulang
+const kelas = "Fosan"
+// kelas = "Soto"
+
+// let jam = "09.00"
+// var menit = "3"
+// console.log(menit);
+
+// jam = "10.00"
+// menit = "59"
+// console.log(menit);
+
+console.log(kelas);
+
+var pesan = "Halo Fosan"
+// var pesan = "Deklarasi ulang"
+pesan = "reassigment"
+
+let message = "Hai"
+// let tidak dapat dideklarasi ulang
+// let message = "Hi"
+
+console.log(pesan);
---
+++ 4.Basic-string.js
@@ -0,0 +1,63 @@
+// 1. Let's Form a Sentence
+let word1 = 'JavaScript';
+let second = 'is';
+let third = 'awesome';
+let fourth = 'and';
+let fifth = 'I';
+let sixth = 'love';
+let seventh = 'it!';
+
+console.log(word1 + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh);
+let Output = `${word1} ${second} ${third} ${fourth} ${fifth} ${sixth} ${seventh}`
+console.log(Output)
+
+
+// 2. Index Accessing - 1 by 1
+let word2 = 'wow JavaScript is so cool';
+let exampleFirstWord2 = word2[0] + word2[1] + word2[2];
+let secondWord = word2[4] + word2[5] + word2[6] + word2[7] + word2[8] + word2[9] + word2[10] + word2[11] + word2[12] + word2[13];
+let thirdWord = word2[15] + word2[16];
+let fourthWord = word2[18] + word2[19];
+let fifthWord = word2[21] + word2[22] + word2[23] + word2[24];
+
+console.log('First Word: ' + exampleFirstWord2);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+
+
+// 3. Breaking Sentence (Again) using Substring
+let word3 = 'wow JavaScript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
+let secondWord3 = word3.substring(4, 14); 
+let thirdWord3 = word3.substring(15, 17); 
+let fourthWord3 = word3.substring(18, 20);
+let fifthWord3 = word3.substring(21, 25); 
+
+console.log('First Word: ' + exampleFirstWord3);
+console.log('Second Word: ' + secondWord3);
+console.log('Third Word: ' + thirdWord3);
+console.log('Fourth Word: ' + fourthWord3);
+console.log('Fifth Word: ' + fifthWord3);
+
+
+// 4. Breaking Sentence (yet Again) and Count Each Length
+let word4 = 'wow JavaScript is so cool';
+let exampleFirstWord4 = word4.substring(0, 3);
+let secondWord4 = word4.substring(4, 14); 
+let thirdWord4 = word4.substring(15, 17); 
+let fourthWord4 = word4.substring(18, 20);
+let fifthWord4 = word4.substring(21, 25);
+
+let firstWordLength = exampleFirstWord4.length;
+let secondWordLength = secondWord4.length;
+let thirdWordLength = thirdWord4.length;
+let fourthWordLength = fourthWord4.length;
+let fifthWordLength = fifthWord4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord4 + ', with length:' + secondWordLength);
+console.log('Third Word: ' + thirdWord4 + ', with length:' + thirdWordLength);
+console.log('Fourth Word: ' + fourthWord4 + ', with length:' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord4 + ', with length:' + fifthWordLength);
